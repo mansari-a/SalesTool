e16789adb6297f2ea7916cec5821356ae91fa6a3
SONY <SONY@VAIO>
2017-12-02 12:25:08 +0530

    hello

--- FAQs/src/com/sc/list/ReverseLinkedList.java
+++ FAQs/src/com/sc/list/ReverseLinkedList.java
@@ -8,17 +8,8 @@ public class ReverseLinkedList {
 	public static void main(String[] args) {
 		
 
-		List<?> l = new ArrayList<Number>();
 		
-		//List<? extends Number> l2 = new ArrayList<Number>();
-		//List<? super Number> l3 = new ArrayList<Number>();
-		//List<Number> l4 = new ArrayList<Number>();
-		
-		//l.add(11, null);
-		
-		l.add(new Integer(11), null);
-		
-		System.out.println(l.get(0));
+		System.out.println("Hello");
 		//A
 	
 	}
